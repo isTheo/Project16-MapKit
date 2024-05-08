cc337f2e23199d0cdeeaad0842162788a7187def
isTheo <matteohg07@gmail.com>
2024-05-08 17:49:05 +0200

    project16 updated

--- Project16-MapKit/Capital.swift
+++ Project16-MapKit/Capital.swift
@@ -6,7 +6,20 @@
 //
 
 import UIKit
+import MapKit
 
-class Capital: NSObject {
-
+class Capital: NSObject, MKAnnotation {
+    var title: String?
+    var coordinate: CLLocationCoordinate2D
+    var info: String
+    
+    init(title: String?, coordinate: CLLocationCoordinate2D, info: String) {
+        self.title = title
+        self.coordinate = coordinate
+        self.info = info
+    }
+    
+    
+    
+    
 }
